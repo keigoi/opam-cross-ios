0b0752b51c514f3dc06f8268727ddd13622d7593
Keigo Imai <keigo.imai@gmail.com>
2020-10-05 16:29:52 +0900

    camlp5-ios

---
+++ packages/camlp5-ios.7.08/files/ios-install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# remove default OCaml from the path and add OCaml-iOS instead
+PATH=$(echo $PATH | sed -e "s|$(opam var bin):||g")
+PATH=$(echo $PATH | sed -e "s|$(opam var prefix)/ios-sysroot/bin:||g")
+export PATH=`opam var prefix`/ios-sysroot/bin:$PATH
+
+./configure --prefix $(opam var prefix)/ios-sysroot
+make -j8 world
+make -j8 optlib
+make install
