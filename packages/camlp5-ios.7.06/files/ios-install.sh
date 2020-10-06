5621413d3f53da945907ddf8f27d03579f6a5819
Keigo Imai <keigo.imai@gmail.com>
2020-10-06 09:58:43 +0900

    camlp5-ios: manually copy META

--- packages/camlp5-ios.7.06/files/ios-install.sh
+++ packages/camlp5-ios.7.06/files/ios-install.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -e
 
-# remove default OCaml from the path and add OCaml-iOS instead
-PATH=$(echo $PATH | sed -e "s|$(opam var bin):||g")
-PATH=$(echo $PATH | sed -e "s|$(opam var prefix)/ios-sysroot/bin:||g")
-export PATH=`opam var prefix`/ios-sysroot/bin:$PATH
-
-./configure --prefix $(opam var prefix)/ios-sysroot
-make -j8 world
-make -j8 optlib
 make install
+
+mkdir -p `opam var prefix`/ios-sysroot/lib/camlp5
+cp etc/META `opam var prefix`/ios-sysroot/lib/camlp5
